e091a19d95f37fb0bc29a03fe372e1ad27d77a09
Ernesto <ernestoaguaysol@gmail.com>
2018-01-26 17:08:46 -0300

    barra y linea

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,16 @@ import { ChartsModule } from 'ng2-charts';
 
 // components
 import { LineaComponent } from './components/linea/linea.component';
+import { BarraComponent } from './components/barra/barra.component';
+import { DonaComponent } from './components/dona/dona.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LineaComponent
+    LineaComponent,
+    BarraComponent,
+    DonaComponent
   ],
   imports: [
     BrowserModule,
